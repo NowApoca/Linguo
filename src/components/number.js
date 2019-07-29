4e907d9d37598e852958876441eacad315e8d1f6
NowApoca <manueldiez123@gmail.com>
2019-07-29 18:48:43 -0300

    Random number

--- src/components/number.js
+++ src/components/number.js
@@ -4,28 +4,38 @@ import {constants} from "../constant"
 
 export class Numbers extends React.Component {
     constructor(props){
-      super(props)
-      this.unMount = props.unMount
-      this.state = {number:""}
+        super(props)
+        this.unMount = props.unMount
+        this.state = {numberInText:"",numberRandom:""}
         this.handleNumberChange = this.handleNumberChange.bind(this);
         this.handleVerify = this.handleVerify.bind(this);
+        this.generateNumber = this.generateNumber.bind(this);
     }
     handleNumberChange(event){
-        this.setState({number: event.target.value});
+        this.setState({numberInText: event.target.value});
+    }
+    generateNumber(){
+        let randNumber = String(Math.floor(Math.random()*constants.maxNumCHN));
+        this.setState({numberRandom: randNumber});
     }
     handleVerify(event){
-        // match with PINYIN    
+        if(this.state.numberInText == this.state.numberRandom){
+
+        }else{
+
+        }
     }
     render() {
         return (
             <div className="App">
                 <button onClick={() => { this.unMount("index")}}>Index</button>
+                <button onClick={() => { this.generateNumber("index")}}>Generate Number Random</button>
                 <h1> Numbers </h1>
                 <h1> From {"-"+constants.maxNumCHN} to {constants.maxNumCHN} </h1>
                 <h1> Numbers </h1>
                 <label>
                 Number:
-                <input type="text" value={this.state.number} onChange={this.handleNumberChange} />
+                <input type="text" value={this.state.numberInText} onChange={this.handleNumberChange} />
                 </label>
                 <button onClick={() => { this.handleVerify()}}>Verify</button>
             </div>);
