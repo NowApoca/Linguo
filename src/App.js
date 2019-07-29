76c0e1106e2542bba4f990cb2dd2355de9920246
NowApoca <manueldiez123@gmail.com>
2019-07-29 16:44:39 -0300

    Working buttons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
 import React from 'react';
 import './App.css';
-import {words} from "./vocabulary/words"
 import {translate} from "./translate"
 
-function App(language = "CH") {
+class Index extends React.Component {
+  constructor(props){
+    super(props)
+    this.unMount = props.unMount
+  }
+render() {
   return (
-    <div className="App">
-      <h1> {translate("welcome","CH")} </h1>
-    </div>
-  );
+  <div className="App">
+    <h1> {translate("welcome","CH")} </h1>
+    <button onClick={() => { this.unMount("number")}}>Number</button>
+  </div>);
+}
+}
+class Numbers extends React.Component {
+  constructor(props){
+    super(props)
+    this.unMount = props.unMount
+  }
+render() {
+  return (
+  <div className="App">
+    <h1> Numbers </h1>
+    <button onClick={() => { this.unMount("index")}}>Index</button>
+  </div>);
+}
+}
+
+class App extends React.Component {
+    constructor(props){
+      super(props)
+      this.state = {mount:props.mount}
+    }
+    unMountChild(nextComponent){
+      this.setState({mount:nextComponent})
+    }
+    nextComponent(){
+      switch(this.state.mount){
+        case "index":
+          return <Index unMount={this.unMountChild.bind(this)}/>
+        case "number":
+            return <Numbers unMount={this.unMountChild.bind(this)}/>
+        case "phrase":
+            return <Index unMount={this.unMountChild.bind(this)}/>
+        default:
+      }
+    }
+  render() {
+    return (
+    <div >
+      {this.nextComponent()}
+    </div>);
+  }
 }
 
 export default App;
