243152fe93d910abcdced443924be4a24d0f5f27
NowApoca <manueldiez123@gmail.com>
2019-07-30 13:20:53 -0300

    Chinese Number converter

--- src/components/number.js
+++ src/components/number.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../App.css';
 import {constants} from "../constant"
+import { translateNumber } from '../translate';
 
 export class Numbers extends React.Component {
     constructor(props){
         super(props)
         this.unMount = props.unMount
-        this.state = {numberInText:"",numberRandom:""}
+        this.state = {numberInText:0,numberRandom:-5}
         this.handleNumberChange = this.handleNumberChange.bind(this);
         this.handleVerify = this.handleVerify.bind(this);
         this.generateNumber = this.generateNumber.bind(this);
@@ -26,6 +27,11 @@ export class Numbers extends React.Component {
         }
     }
     render() {
+        let translation;
+        if(this.state.numberRandom > -1){
+            translation = translateNumber(this.state.numberRandom,"NUMCHN")
+        }
+        console.log(this.state.numberRandom)
         return (
             <div className="App">
                 <button onClick={() => { this.unMount("index")}}>Index</button>
@@ -33,6 +39,11 @@ export class Numbers extends React.Component {
                 <h1> Numbers </h1>
                 <h1> From {"-"+constants.maxNumCHN} to {constants.maxNumCHN} </h1>
                 <h1> Numbers </h1>
+                {(this.state.numberRandom < 0) ? (
+                    ""
+                        ) : (
+                            <h1> {translation} </h1>
+                )}
                 <label>
                 Number:
                 <input type="text" value={this.state.numberInText} onChange={this.handleNumberChange} />
--- src/translate.js
+++ src/translate.js
@@ -1,5 +1,6 @@
 import {wordsPinCh} from "./vocabulary/pinyinChr"
 import {wordsEngPin} from "./vocabulary/englishPinyin"
+import {numberChr,numberPin} from "./vocabulary/numberChn"
 
 export function translate(word,language){
     let translation;
@@ -11,8 +12,69 @@ export function translate(word,language){
             let engToPin = wordsEngPin[word]
             translation = wordsPinCh[engToPin]
             break;
+        case "NUMCHN":
+            translation = numberChr[word]
+            break;
         default: 
             translation = ""
     }
     return translation;
+}
+
+export function translateNumber(number,language){
+    let translatedNumber = ""
+    switch(language){
+        case "PIN":
+            // translation = wordsEngPin[word]
+            break;
+        case "NUMCHN":
+            let decimals = number.length
+            number = parseInt(number);
+            for(let n = 0; n < (decimals) ;n+=1){
+                let digit = ""
+                let digitRes = Math.trunc((number/(10**n))%10)
+                if(digitRes !== 0){
+                    switch(10**n){
+                        case 1:
+                                digit = translate(digitRes,language)
+                        break;
+                        case 10:
+                                digit = translate(digitRes,language) + translate("10",language)
+                        break;
+                        case 100: 
+                                digit = translate(digitRes,language) + translate("100",language)
+                        break;
+                        case 1000:
+                                digit = translate(digitRes,language) + translate("1000",language)
+                        break;
+                        case 10000:
+                                digit = translate(digitRes,language) + translate("10000",language)
+                        break;
+                        case 100000:
+                                digit = translate(digitRes,language) + translate("10",language)
+                        break;
+                        case 1000000:
+                                digit = translate(digitRes,language) + translate("100",language)
+                        break;
+                        case 10000000:
+                                digit = translate(digitRes,language) + translate("1000",language)
+                        break;
+                        case 100000000:
+                                digit = translate(digitRes,language) + translate("100000000",language)
+                        break;
+                        default:
+                    }
+                }
+                if(digitRes === 0 && decimals === 1){
+                    return translate("0",language)
+                }
+                console.log(digit, digitRes, number)
+                translatedNumber = digit + translatedNumber
+            }
+            break;
+        default: 
+        translatedNumber = ""
+    }
+    console.log(translatedNumber)
+    return translatedNumber;
 }
