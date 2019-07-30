67f82b3cb0d9b90e7903710566056673841cbaab
NowApoca <manueldiez123@gmail.com>
2019-07-30 15:59:15 -0300

    Added bootstrap and max min

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import {translate} from "./translate"
 import {Index} from "./components/index"
 import {Numbers} from "./components/number"
+import {Dropdown} from "react-bootstrap"
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends React.Component {
     constructor(props){
       super(props)
-      this.state = {mount:props.mount}
+      this.state = {mount:props.mount,language:"eng"}
     }
     unMountChild(nextComponent){
       this.setState({mount:nextComponent})
@@ -15,17 +17,28 @@ class App extends React.Component {
     nextComponent(){
       switch(this.state.mount){
         case "index":
-          return <Index unMount={this.unMountChild.bind(this)}/>
+          return <Index unMount={this.unMountChild.bind(this)} language={this.state.language}/>
         case "number":
-            return <Numbers unMount={this.unMountChild.bind(this)}/>
+            return <Numbers unMount={this.unMountChild.bind(this)} language={this.state.language}/>
         case "phrase":
-            return <Index unMount={this.unMountChild.bind(this)}/>
+            return <Index unMount={this.unMountChild.bind(this)} language={this.state.language}/>
         default:
       }
     }
   render() {
     return (
     <div >
+      <Dropdown>
+  <Dropdown.Toggle variant="success" id="dropdown-basic">
+    Dropdown Button
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
       {this.nextComponent()}
     </div>);
   }
--- src/components/number.js
+++ src/components/number.js
@@ -7,16 +7,20 @@ export class Numbers extends React.Component {
     constructor(props){
         super(props)
         this.unMount = props.unMount
-        this.state = {numberInText:0,numberRandom:-1,answered:false,response:false}
+        this.state = {numberInText:0,numberRandom:-1,answered:false,response:false,maxNumber:100000000,language:props.language}
         this.handleNumberChange = this.handleNumberChange.bind(this);
+        this.handleMaxNumberChange = this.handleMaxNumberChange.bind(this);
         this.handleVerify = this.handleVerify.bind(this);
         this.generateNumber = this.generateNumber.bind(this);
     }
     handleNumberChange(event){
         this.setState({numberInText: event.target.value});
     }
+    handleMaxNumberChange(event){
+        this.setState({maxNumber: parseInt(event.target.value)});
+    }
     generateNumber(){
-        let randNumber = String(Math.floor(Math.random()*constants.maxNumCHN));
+        let randNumber = String(Math.floor(Math.random()*this.state.maxNumber));
         this.setState({numberRandom: randNumber});
     }
     handleVerify(){
@@ -31,18 +35,22 @@ export class Numbers extends React.Component {
         if(this.state.numberRandom > -1){
             translation = translateNumber(this.state.numberRandom,"NUMCHN")
         }
-        console.log(this.state.numberRandom)
+        console.log(this.state.numberRandom,this.state.language)
         return (
             <div className="App">
                 <button onClick={() => { this.unMount("index")}}>Index</button>
                 <button onClick={() => { this.generateNumber("index")}}>Generate Number Random</button>
                 <h1> Numbers </h1>
-                <h1> From {"-"+constants.maxNumCHN} to {constants.maxNumCHN} </h1>
+                <h1> From {"-"+this.state.maxNumber} to {this.state.maxNumber +". Max number allowed: "+constants.maxNumCHN} </h1>
                 {(this.state.numberRandom < 0) ? (
                     ""
                         ) : (
                             <h1> {translation} </h1>
                 )}
+                <label>
+                Max Number:
+                <input type="text" value={this.state.maxNumber} onChange={this.handleMaxNumberChange} />
+                </label>
                 <label>
                 Number:
                 <input type="text" value={this.state.numberInText} onChange={this.handleNumberChange} />
